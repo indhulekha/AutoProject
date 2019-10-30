ed26d92dff1817e1a48f240d1d50bebba8630cfb
Indulekha <indhulekhagaekwade@gmail.com>
2019-10-30 16:13:26 +0530

    commit autoproject

---
+++ AutoProject/src/com/Demo/Genkin1.java
@@ -0,0 +1,13 @@
+package com.Demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Genkin1 
+{
+	@Test
+	public void Test1()
+	{
+		Reporter.log("hi",true);
+	}
+}
